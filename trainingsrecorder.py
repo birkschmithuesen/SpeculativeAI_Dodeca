a0828f7f035e7a3905ab153fbf2888342bea4b25
Marcus Ding <marcusding@me.com>
2019-07-16 22:53:08 +0200

    Add test and fix trainingsrecorder.py

--- trainingsrecorder.py
+++ trainingsrecorder.py
@@ -11,10 +11,11 @@ import os
 import threading
 import signal
 import sys
+import platform
 import csv
 from pythonosc import osc_server, dispatcher
 from conversation.vision_camera import Camera
-from conversation import neuralnet_dictionary, configuration, vision_camera
+from conversation import neuralnet_vision, configuration, vision_camera
 
 OSC_IP_ADDRESS = "0.0.0.0"
 OSC_PORT = 8005
@@ -46,7 +47,7 @@ def process_trainingsset():
     512 dim vector based on the neural net and saves them together
     with the sound vector to the trainingsset_final list
     """
-    MODEL = neuralnet_dictionary.build_model()
+    MODEL = neuralnet_vision.build_model()
     MODEL.summary()
     for set in trainingsset:
         soundvector = set [0]
@@ -56,7 +57,7 @@ def process_trainingsset():
         if SHOW_FRAMES:
             vision_camera.cv2.imshow('frame', cv2_img)
             key = vision_camera.cv2.waitKey(20)
-        activation_vectors, header, img_coll_bn = neuralnet.get_activations(\
+        activation_vectors, header, img_coll_bn = neuralnet_vision.get_activations(\
             MODEL, img_collection, names_of_file)
         trainingsset_final.append((activation_vectors, soundvector))
     print("Finished processing trainings set")
@@ -109,17 +110,27 @@ def stop_recording():
         stop_event.set()
     threading.Thread(target=stop, daemon=True).start()
 
-if __name__ == "__main__":
-    dispatcher = dispatcher.Dispatcher()
-    dispatcher.map("/record_sound_vector", record)
-    dispatcher.map("/stop", osc_stop)
+def start_recording():
+    """
+    Execute the trainingsrecorder
+    """
+    global server
+    dispatcher_server = dispatcher.Dispatcher()
+    dispatcher_server.map("/record_sound_vector", record)
+    dispatcher_server.map("/stop", osc_stop)
     server = osc_server.ThreadingOSCUDPServer(
-        (OSC_IP_ADDRESS, OSC_PORT), dispatcher)
+        (OSC_IP_ADDRESS, OSC_PORT), dispatcher_server)
     print("Serving on {}".format(server.server_address))
     threading.Thread(target=server.serve_forever, daemon=True).start()
-    signal.signal(signal.SIGINT, signal_handler)
+
+if __name__ == "__main__":
+    start_recording()
+    if any(platform.win32_ver()):
+        signal.signal(signal.CTRL_C_EVENT, signal_handler)
+    else:
+        signal.signal(signal.SIGINT, signal_handler)
     print('Press Ctrl+C to save current data to disk.')
     stop_event.wait()
-    #process_trainingsset()
+    process_trainingsset()
     save_to_disk()
     sys.exit(0)
