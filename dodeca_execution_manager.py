fa9ba0b7897742f185d55d4fe0fc5e6675a99cbc
Marcus Ding <marcusding@icloud.com>
2020-01-18 00:31:59 +0100

    Improved and testet dodeca_execution_manager.py

--- dodeca_execution_manager.py
+++ dodeca_execution_manager.py
@@ -1,7 +1,8 @@
 """
 This script manages the execution and error handling of the sound synthesis
-software SuperCollider in case the sound card is disconnected before
-or after start of the host machine
+software SuperCollider in case the sound card or camera is disconnected before
+or after start of the host machine or has any other issues that stops
+the programs.
 
 Make sure, that the user running this script is allowed
 to run sudo passwordless sudo commands for reboot and usbreset.
@@ -9,10 +10,11 @@ to run sudo passwordless sudo commands for reboot and usbreset.
 
 import threading
 import subprocess
-from time import sleep
-from queue import Queue
+from time import time, sleep
 
-N_USB_DEVICE_CHECKS = 7
+N_USB_DEVICE_CHECKS = 3
+WAIT_TIME_BEFORE_SUPERCOLLIDER_START_SECONDS = 0
+EXECUTION_ATTEMPTS_RESET_TIMEOUT_SECONDS = 600
 
 def usb_device_is_connected(device_string):
     """
@@ -77,9 +79,10 @@ def execute_supercollider():
     """
     run supercollider in background and return POpen object
     """
+    sleep(WAIT_TIME_BEFORE_SUPERCOLLIDER_START_SECONDS)
     cmd = "sclang /home/dodeca/Documents/SpeculativeAI_Dodeca/data/sound_synthesis/"\
             "SAI_soundSynthesis_04_masterEFX.scd"
-    return subprocess.Popen(cmd.split(), stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
+    return subprocess.Popen(cmd.split(), stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
 
 def execute_dodeca_main():
     """
@@ -87,7 +90,7 @@ def execute_dodeca_main():
     """
     print("Starting dodeca main")
     cmd = "python3 /home/dodeca/Documents/SpeculativeAI_Dodeca/main.py"
-    return subprocess.Popen(cmd.split(), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    return subprocess.Popen(cmd.split(), stderr=subprocess.DEVNULL)
 
 def execute_supercollider_dodeca_main():
     """
@@ -95,35 +98,61 @@ def execute_supercollider_dodeca_main():
     if one fails return
     """
     print("Starting SuperCollider")
+    start_time = time()
     dodeca_main = execute_dodeca_main()
     supercollider = execute_supercollider()
     def check_supercollider_stdout():
-        for line in iter(supercollider.stderr.readline, b""):
-            if b"JackAudioDriver::ProcessAsync: read error, stopping..." in line:
+        for line in iter(supercollider.stdout.readline, b""):
+            if b"JackTemporaryException" in line:
                 print("JackAudioDriver stopped")
+                print(line)
                 break
             if b"Cannot initialize driver" in line:
-                print("JackaudioDriver wasn't initialized")
+                print("JackAudioDriver couldn't be started")
+                print(line)
                 break
+            if b"JackSocketClientChannel read fail" in line:
+                print("JackAudioDriver couldn't be started")
+                print(line)
+                break
+            if b"Server unresponsive:true" in line:
+                print("JackAudioDriver stopped")
+                print(line)
+                break
+    supercollider_stdout_thread = threading.Thread(target=check_supercollider_stdout)
+    supercollider_stdout_thread.start()
     def shutdown_execution():
         print("Shutting down execution of SuperCollider and dodeca main")
         dodeca_main.kill()
         supercollider.kill()
-    supercollider_stdout_thread = threading.Thread(target=check_supercollider_stdout)
-    supercollider_stdout_thread.start()
+        subprocess.run("killall jackd".split()) # necessary if we want to restart supercollider later
     while True:
         if dodeca_main.poll() is None and supercollider.poll() is None\
                 and supercollider_stdout_thread.is_alive():
             continue
+        print("One of the processes died.")
+        print("Alive: dodeca_main: {} supercollider {} supercollider_stdout_thread: {}".format(dodeca_main.poll() is None, supercollider.poll() is None, supercollider_stdout_thread.is_alive()))
         shutdown_execution()
-        return
+        return time() - start_time
 
 def main():
+    """
+    first we check that the USB devices are visible, if yes we execute the software
+    and monitor it's health. If one program dies, we check that the USB devices are visible
+    and reset them if needed. If nothing helps we reboot the host.
+    """
     print("Starting Dodeca Manager")
-    ensure_devices_are_connected()
+    execution_attempts = 0
     while True:
-        execute_supercollider_dodeca_main()
+        execution_attempts += 1
         ensure_devices_are_connected()
+        if execution_attempts >= N_USB_DEVICE_CHECKS:
+            usb_reset_multcheck()
+        if execution_attempts >= 2*N_USB_DEVICE_CHECKS:
+            subprocess.Popen("sudo reboot".split())
+        time_elapsed = execute_supercollider_dodeca_main()
+        if time_elapsed > EXECUTION_ATTEMPTS_RESET_TIMEOUT_SECONDS:
+            execution_attempts = 0
 
 if __name__ == "__main__":
     main()
